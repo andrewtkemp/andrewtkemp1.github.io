bcf746947a421583caa420e53fee3d479ae8602b
Andrew Kemp <andrewtkemp@gmail.com>
2018-09-16 12:52:15 -0500

    Shift nav bar to display portfolio tiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 const home = document.getElementById("lol-jumbotron");
 const overlay = document.getElementById("overlay");
+const portfolio = document.getElementById("portfolio");
+portfolio.addEventListener("click", function () {
+    if(home.classList[1] === "shift"){
+        home.classList.remove("shift");
+        return true;
+    }
+    home.classList.add("shift");
+})
 const loading = function (isLoading) {
     if (isLoading) {
         overlay.classList.add("loaded");
@@ -7,22 +15,7 @@ const loading = function (isLoading) {
     }
     overlay.classList.remove("loaded");
 }
-const renderDiv = function (project) {
-    let comrade = document.getElementById('lol-jumbotron');
-    let portfolioItem = "<div class='portfolio-item'>";
-    portfolioItem += `<a href=${project.url} target="_blank"><div id=${project.id} class="image-div media"><img src="./assets/images/${project.image}"/>`
-    portfolioItem += `<div class="media__body">${project.name}</div></div></a>`
-    portfolioItem += "</div>"
-    comrade.insertAdjacentHTML('afterend', portfolioItem);
-}
 const showNav = function () {
     home.classList.add("loaded");
 }
-const showPortfolio = function (event) {
-    let id = event.innerText;
-    for (let i = 0; i < projects.length; i++) {
-        renderDiv(projects[i]);
-    }
-}
-
 setTimeout(showNav, 1000);
