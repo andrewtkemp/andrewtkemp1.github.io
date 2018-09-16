b34185c742ddc445ec69607fde6782a10ba93afc
Andrew Kemp <andrewtkemp@gmail.com>
2018-09-16 02:08:29 -0500

    Portfolio Grid slowly coming together.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,28 @@
-window.onload = function(){
-    var home = document.getElementById("lol-jumbotron");
-    home.classList.add("loaded");
+const home = document.getElementById("lol-jumbotron");
+const overlay = document.getElementById("overlay");
+const loading = function (isLoading) {
+    if (isLoading) {
+        overlay.classList.add("loaded");
+        return true;
+    }
+    overlay.classList.remove("loaded");
 }
-
-var renderDiv = function(){
-    var comrade = document.getElementById('lol-jumbotron'); 
-    var portfolioItem = "<div class='portfolio-item'>";
-    portfolioItem += "<a href='https://google.com'>"
-    portfolioItem += "Portfolio Item"
-    portfolioItem += "</a>"
+const renderDiv = function (project) {
+    let comrade = document.getElementById('lol-jumbotron');
+    let portfolioItem = "<div class='portfolio-item'>";
+    portfolioItem += `<a href=${project.url} target="_blank"><div id=${project.id} class="image-div media"><img src="./assets/images/${project.image}"/>`
+    portfolioItem += `<div class="media__body">${project.name}</div></div></a>`
+    portfolioItem += "</div>"
     comrade.insertAdjacentHTML('afterend', portfolioItem);
 }
-var showPortfolio = function(event){
-    var id = event.innerText;
-    var delay = 1000
-    var portfolioLength = 10;
-    for(var i = 0; i < portfolioLength; i++){
-        delay = delay + 1000;
-        setTimeout(renderDiv, delay)
+const showNav = function () {
+    home.classList.add("loaded");
+}
+const showPortfolio = function (event) {
+    let id = event.innerText;
+    for (let i = 0; i < projects.length; i++) {
+        renderDiv(projects[i]);
     }
 }
+
+setTimeout(showNav, 1000);
---
+++ assets/js/resume.js
@@ -0,0 +1,23 @@
+var projects = [
+    {
+        name:"Caliber Collision",
+        id:"caliber-collision",
+        image:"CaliberCollision.png",
+        url:"https://calibercollision.com/",
+        technologies:["VueJS", "Laravel", "Statamic 2 CMS"]
+    },
+    {
+        name:"Caliber Collision - Blog",
+        id:"caliber-collision-blog",
+        image:"CaliberBlog.png",
+        url:"https://blog.calibercollision.com/",
+        technologies:["Ghost Blog"]
+    },
+    {
+        name:"Caliber Collision - Estimate",
+        id:"caliber-collision-estimate",
+        image:"CaliberEstimate.png",
+        url:"https://estimate.calibercollision.com/",
+        technologies:["PHP", "jQuery"]
+    }
+]
