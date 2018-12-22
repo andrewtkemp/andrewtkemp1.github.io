a46d68d98306b041214bfec50c9801eacf44f611
Andrew Kemp <andrewtkemp@gmail.com>
2018-12-22 12:31:50 -0600

    Clean Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-const home = document.getElementById("lol-jumbotron");
-const overlay = document.getElementById("overlay");
-const portfolio = document.getElementById("portfolio");
-portfolio.addEventListener("click", function () {
-    loading(true);
-})
-const loading = function (isLoading) {
-    if (isLoading) {
-        overlay.classList.add("loaded");
-        return true;
-    }
-    overlay.classList.remove("loaded");
+var home = document.getElementById("lol-jumbotron");
+var navLinks = document.getElementsByClassName("nav-link");
+var contentContainer = document.getElementById("content-container")
+var getContentType = function () {
+    var contentType = this.getAttribute("data-id");
+    console.log("Content Type Is ", contentType)
+    contentContainer.querySelector("h3").innerHTML = contentType
+    contentContainer.classList.add("loaded")
 }
-const showNav = function () {
+for(var i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener("click", getContentType, false)
+}
+
+
+var showNav = function () {
     home.classList.add("loaded");
 }
 setTimeout(showNav, 1000);
